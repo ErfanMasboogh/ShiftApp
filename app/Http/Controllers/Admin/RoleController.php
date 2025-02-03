925a91e04d4acf8cb85f50ffd89639159721c526
ErfanMasboogh <erfaanmasbogh@gmail.com>
2025-02-03 21:17:19 +0330

    improve: readability in RoleController

--- app/Http/Controllers/Admin/RoleController.php
+++ app/Http/Controllers/Admin/RoleController.php
@@ -6,11 +6,15 @@ use App\Http\Controllers\Controller;
 use App\Models\Role;
 use App\Models\User;
 use Illuminate\Http\Request;
+use App\Http\Requests\Role\UpdateRequest;
+use App\Http\Requests\Role\StoreRequest;
+use Illuminate\Http\RedirectResponse;
 use Inertia\Inertia;
+use Inertia\Response as InertiaResponse;
 
 class RoleController extends Controller
 {
-    public function list()
+    public function list(): InertiaResponse
     {
         $roles = Role::all();
         $context = [
@@ -18,10 +22,10 @@ class RoleController extends Controller
         ];
         return Inertia::render('Admin/Users/Roles', compact('context'));
     }
-    public function delete($id)
+    public function delete($id): RedirectResponse
     {
         if ($id == 1) {
-            return redirect(route('admin.users.roles'));
+            return redirect()->route('admin.users.roles');
         }
         $users = User::where('role_id', $id)->get();
         foreach ($users as $user) {
@@ -29,29 +33,20 @@ class RoleController extends Controller
             User::updateUser($user);
         }
         Role::deleteRole($id);
-        return redirect(route('admin.users.roles'));
+        return redirect()->route('admin.users.roles');
     }
-    public function update(Request $request)
+    public function update(UpdateRequest $request): RedirectResponse
     {
-        $request->validate([
-            'name' => 'required|string|max:64',
-            'over_payment' => 'required|max:4'
-        ]);
         $role = Role::find($request->id);
         $role->name = $request->name;
         $role->over_payment = $request->over_payment;
         Role::updateRole($role);
-        return redirect(route('admin.users.roles'));
+        return redirect()->route('admin.users.roles');
     }
-
-    public function store(Request $request)
+    public function store(StoreRequest $request): RedirectResponse
     {
-        $request->validate([
-            'name' => 'required|string|max:32',
-            'over_payment' => 'required|max:4'
-        ]);
         Role::createRole($request->name,$request->over_payment);
-        return redirect(route('admin.users.roles'));
+        return redirect()->route('admin.users.roles');
     }
 
 }
